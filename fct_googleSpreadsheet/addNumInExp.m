3c8b3215d4bb93e4465f74fcd4034a4e156026f8
Corinna504 <corinn_lorenz@gmx.de>
2017-05-23 14:56:08 +0200

    added comments & use of contains.mat

--- fct_googleSpreadsheet/addNumInExp.m
+++ fct_googleSpreadsheet/addNumInExp.m
@@ -1,9 +1,27 @@
 function exinfo = addNumInExp( exinfo )
+% exinfo = addNumInExp( exinfo )
+% 
 % loads the spike sorting tables and reads out the number of 5HT
 % experiments within the unit recording for each drug experiment
 %
-%
-%
+% Following fields are added:
+%   dn_id           - the number of preceeding drug experiments before the 
+%                       current one during the recording of this unit
+%   dn_session      -the number of drug experiments before the current one
+%                       during the recording session (including other
+%                       unit recordings)
+%   dt_id           - the absolut time between this and the beginning of
+%                       the first drug experiment in the recording of this
+%                       unit before the current experiment
+%   dt_session      - the absolut time between this and the beginning of
+%                       the first drug experiment in this recording session
+%   dt_cum_id       - the cummulated time of drug application in the
+%                       recording of this unit before the given experiment
+%   dt_cum_session  - the cummulated time of drug application before the 
+%                       given experiment in the recording session 
+% 
+% 
+% 
 % @CL February 2, 2017
 % added the temporal distance to the first experiment in the recording
 % session
@@ -17,6 +35,8 @@ sorting.matfilename = [{B.matfilename}'; {A.matfilename}'];
 sorting.iontophoresis = [{B.iontophoresis}'; {A.iontophoresis}'];
 
 
+% loop throught the result structure and add retract the information about
+% preceeding events
 for i = 1:length(exinfo)
     
     fprintf('WORKING ON ROW %1i, file %1.1f \n', i, exinfo(i).id);
@@ -35,18 +55,20 @@ end
 
 %%
 function [dn, dt, dt_cum] = getDeltaNexp(exinfo, sorting)
-% for each filename fname look for the according entry in the sorting file
-% if it is a concatenated file, take the average sorting quality
+% for each filename fname look for the according entry in the sorting file.
+% count the number of experiment with 5HT application before this
+% experiment within unit (dn_id) and session (dn_session) as well as the
+% absolute time to the first 5HT experiment (dt_id, dt_session) and the
+% cummulative time of 5HT application (dt_cum_id, dt_cum_session)
 
 
-fname = exinfo.fname_drug;
-id = floor(exinfo.id); % #unit
+fname = exinfo.fname_drug; % the current file
+id = floor(exinfo.id); % unit id
 monkey = exinfo.monkey; % animal name
 drugname = exinfo.drugname; % can be 5HT or NaCl  
 
-if isempty(strfind(fname, 'all.grating'))
-    [dn, dt, dt_cum] = getDeltaNexpHelper(fname, id, monkey, sorting, drugname);
-else
+if contains(fname, 'all.grating')
+    
     % loop through all original experiments, if it is a concatenated file
     load(fname);
     fname_list = getFnames(ex);
@@ -54,10 +76,15 @@ else
         [dn_list(kk), dt_list(kk), dt_cum_list(kk)] = ...
             getDeltaNexpHelper(fname_list{kk}, id, monkey, sorting, drugname);
     end
+     
+    % use the minimum distance between experiments, the minimum time, etc.
+    % therefore handling the concatenated experiments as one
     dn.id = min([dn_list.id]);  dn.session = min([dn_list.session]);
     dt.id = min([dt_list.id]);  dt.session = min([dt_list.session]);
     dt_cum.id = min([dt_cum_list.id]);   dt_cum.session = min([dt_cum_list.session]);
     
+else
+    [dn, dt, dt_cum] = getDeltaNexpHelper(fname, id, monkey, sorting, drugname);
 end
 
 end
@@ -72,30 +99,30 @@ function [dn, dt, dt_cum] = getDeltaNexpHelper(fname, id, monkey, sorting, drugn
 idx_id = findIdxMatchingID(sorting.matfilename, id, monkey);
 
 % use only the drug applied experiments
-idx_id = idx_id( ~cellfun(@isempty, strfind(sorting.iontophoresis(idx_id), drugname)) );
+idx_id = idx_id( contains(sorting.iontophoresis(idx_id), drugname) );
 
-% find the index of this exact experiment under observation
-idx_f = findIdxMatchingFname(sorting.matfilename, fname, monkey);
+% find the index of the current experiment under observation
+idx_cex = findIdxMatchingFname(sorting.matfilename, fname, monkey);
 
 
 % find the session related drug experiments
 % use the recording data as indication of a session begin...
-idx_session = find( ~cellfun(@isempty, sorting.recordingdate(1:idx_f)) , 1, 'last');
+idx_session = find( ~cellfun(@isempty, sorting.recordingdate(1:idx_cex)) , 1, 'last');
 % identify all the rows up to the current experiment...
-idx_session = idx_session:idx_f;
+idx_session = idx_session:idx_cex;
 % and extract the drug experiments
-idx_session = idx_session( ~cellfun(@isempty, strfind(sorting.iontophoresis(idx_session), drugname)) );
+idx_session = idx_session( contains(sorting.iontophoresis(idx_session), drugname) );
 
 
    
 fprintf('ex of interest: %1.0f \n 1st ex in unit: %1.0f \n 1st ex in session: %1.0f \n\n', ...
-    idx_f, idx_id(1), idx_session(1));
+    idx_cex, idx_id(1), idx_session(1));
 
 %%% compute the number of drug experiments between the session start and
 %%% the current experiment
-% for this we only need the difference between the indices accounting
-dn.id = sum( idx_id < idx_f );
-dn.session = sum( idx_session < idx_f );
+% for this we only need the difference between the indices 
+dn.id = sum( idx_id < idx_cex );
+dn.session = sum( idx_session < idx_cex );
 
 
 %%% compute the time between first 5HT application and the current
@@ -109,7 +136,7 @@ dn.session = sum( idx_session < idx_f );
 % and the cummulative time of drug application
     function [dt, dt_cum] = getDeltaT(idx)
         
-        if idx(1) == idx_f
+        if idx(1) == idx_cex
             dt = 0; % if this is the first experiment
             dt_cum = 0;
         else
@@ -132,7 +159,7 @@ dn.session = sum( idx_session < idx_f );
             
             %%% the cummulative time of drug application before the start of this
             %%% exeriment
-            idx = idx(idx<idx_f);
+            idx = idx(idx<idx_cex);
             dt_cum = 0;
             for kk =1:length(idx)
                 ex_fname = fulfillfdir(sorting.matfilename{idx(kk)}, monkey,...
@@ -167,6 +194,9 @@ fname_ful = [fname_short(1:i_end+1) 'c1_sortLH_' fname_short(i_end+2:end) '_' dr
 % concatenate directory and filename
 fname = fullfile(fdir, fname_ful);
 
+
+% this is hard coded alrorithms to check for alternative filenames that
+% are valid 
 if ~exist(fname, 'file')
     fname = strrep(fname, 'XX', 'CO');
 end
@@ -195,7 +225,6 @@ if ~exist(fname, 'file')
 end
 
 
-
 if ~exist(fname, 'file')
     fname
     error('this file does not exist as it is');
@@ -207,30 +236,22 @@ end
 
 %%
 function [t_strt, t_end] = getTstart(ex)
+% returns the time stamp of the start of the first trial and the end of the
+% last trial
 
-% if isfield(ex, 'Header')
-%     if isfield(ex.Header, 'fileID')
-%         t = ex.Header.fileID;
-%     elseif isfield(ex.Header, 'Sort')
-%         t = ex.Header.Sort.DateTime;
-%     end
-% else
-%     disp('search for alternatives');
-% end
 t_strt = ex.Trials(1).TrialStart;
 t_end = ex.Trials(end).TrialEnd;
-
-% t = datestr(t);
-% t = datevec(t);
 
 end
 
 
-function id = getID(fname)
+function id = getID(exname)
+% retracts the unit ID from the ex filename
+% exname is the identifier from the google spreadsheet
+% example: ma_0322_16.05.grating.SF.mat
 
-idx = strfind(fname, '_');
-
-id = str2double(fname(idx(1)+1:idx(2)-1));
+idx = strfind(exname, '_');
+id = str2double(exname(idx(1)+1:idx(2)-1));
 
 
 end
