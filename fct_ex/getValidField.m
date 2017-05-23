c950892262c6ecd84beed2e119fdabb742609a0e
Corinna504 <corinn_lorenz@gmx.de>
2017-05-23 14:55:07 +0200

    added comments

--- fct_ex/getValidField.m
+++ fct_ex/getValidField.m
@@ -1,9 +1,20 @@
 function exinfo = getValidField( exinfo )
+% exinfo = getValidField( exinfo )
+% 
+% adds a new field valid that identifies the experiment pair with the best
+% regression fit for each unit across the same stimulus dimension.
+% 
+% 
+% @CL
+%
 
-%---------------------------------------------- choose one trial per neuron
-valid  = zeros(1, length(exinfo));
-intracmp = zeros(1, length(exinfo));
 
+%intitialize the variable carrying the boolean values that identify the
+%experiment pairs with the best regression fits per unit and tested
+%stimulus dimension
+hasbestregfit  = false(1, length(exinfo));
+
+% loop through all units
 for neuron = unique([exinfo.id])
     
     neuron_idx = [exinfo.id] == neuron ...
@@ -18,39 +29,40 @@ for neuron = unique([exinfo.id])
     %%%
     if any(or_idx)
         [~, i] = max([exinfo(or_idx).r2reg]);
-        valid(or_idx(i)) = 1;
+        hasbestregfit(or_idx(i)) = 1;
     end
     
     %%%
     if any(rc_idx)
         [~, i] = max([exinfo(rc_idx).r2reg]);
-        valid(rc_idx(i)) = 1;
+        hasbestregfit(rc_idx(i)) = 1;
     end
     
     %%%
     if any(sf_idx)
         [~, i] = max([exinfo(sf_idx).r2reg]);
-        valid(sf_idx(i)) = 1;
+        hasbestregfit(sf_idx(i)) = 1;
     end
     
     %%%
     if any(co_idx)
         [~, i] = max([exinfo(co_idx).r2reg]);
-        valid(co_idx(i)) = 1;
+        hasbestregfit(co_idx(i)) = 1;
     end
     
     %%%
     if any(sz_idx)
         [~, i] = max([exinfo(sz_idx).r2reg]);
-        valid(sz_idx(i)) = 1;
+        hasbestregfit(sz_idx(i)) = 1;
     end
     
     
 end
 
-
-valid        = num2cell(valid==1);
-[exinfo.valid] = deal(valid{:});
+% assign the best regression fit identifiers as field to the result
+% structure
+hasbestregfit        = num2cell(hasbestregfit==1);
+[exinfo.valid] = deal(hasbestregfit{:});
 
 
 end
