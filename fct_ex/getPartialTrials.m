55dd8a15a172776cdd15351df92319b9b2fcf671
Corinna504 <corinn_lorenz@gmx.de>
2017-05-24 11:39:43 +0200

    added comments

--- fct_ex/getPartialTrials.m
+++ fct_ex/getPartialTrials.m
@@ -1,8 +1,13 @@
-function A = getPartialTrials(A)
+function A = getPartialTrials(A, n)
 % divides the struct A into n parts and returns the elements of the last part
+% n is an optinal argument. the default value is 2.
+%
+% @CL 
 
 
-n = 2;
+if nargin==1
+    n = 2;
+end
 
 j = ceil(length(A)/n);
 A = A(j:end);
