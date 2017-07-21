251baadbbeb0341f1c133d56ab4f049d60faeeed
Corinna504 <corinn_lorenz@gmx.de>
2017-07-21 11:19:09 +0200

    added the modulation as criteria for data plotting

--- GuiHelper/markerFaceAssignment.m
+++ GuiHelper/markerFaceAssignment.m
@@ -1,10 +1,14 @@
 function col = markerFaceAssignment( exinfo )
 % assign face color according to applied drug
 
-if exinfo.is5HT
-    col = 'r';
-elseif ~exinfo.is5HT
-    col = 'k';
+if exinfo.pmodulation(2)<0.05
+    if exinfo.is5HT
+        col = 'r';
+    elseif ~exinfo.is5HT
+        col = 'k';
+    end
+else
+    col = 'w';
 end
     
 end
